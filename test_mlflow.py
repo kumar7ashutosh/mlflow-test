879a4e2603f3cff3991fb55649c6c04c8abcb04f
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-12-12 23:17:50 +0530

    mlflow test

--- test_mlflow.py
+++ test_mlflow.py
@@ -6,13 +6,16 @@ from sklearn.datasets import load_wine
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
-from dagshub.auth import login
+from dagshub.auth import get_authenticator
 # ========= DAGSHUB AUTHENTICATION =========
 # Requires 2 environment variables:
 #   DAGSHUB_USERNAME  → your Dagshub username
 #   CAPSTONE_TEST     → your Dagshub token
-login(os.getenv("DAGSHUB_USERNAME"), os.getenv("CAPSTONE_TEST"))
-
+auth = get_authenticator()
+auth.login(
+    username=os.getenv("DAGSHUB_USERNAME"),
+    password=os.getenv("CAPSTONE_TEST")
+)
 # ========= TRACKING URI SETUP =========
 repo_owner = "kumarashutoshbtech2023"
 repo_name = "mlflow-test"
